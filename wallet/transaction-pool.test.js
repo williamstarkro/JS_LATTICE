25ae979ff1a3fefeec3e693c59c6189a47a85b29
williamstarkro <wcs12@pitt.edu>
2018-04-08 21:24:44 -0400

    fully functioning ledger

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -1,17 +1,18 @@
 const TransactionPool = require('./transaction-pool');
 const Transaction = require('./Transaction');
 const Wallet = require('./Index');
+const Blockchain = require('../blockchain');
 
 describe('TransactionPool', () => {
 
-	let tp, wallet, transaction;
+	let tp, wallet, transaction, bc;
 
 	beforeEach(() => {
 
 		tp = new TransactionPool();
+		bc = new Blockchain();
 		wallet = new Wallet();
-		transaction = Transaction.newTransaction(wallet, 'r4nd=4dr355', 30);
-		tp.updateOrAddTransaction(transaction);
+		transaction = wallet.createTransaction('r4nd-4ddr355', 40, bc, tp);
 	});
 
 	it('prove transaction adds to transaction pool itself', () => {
@@ -28,5 +29,39 @@ describe('TransactionPool', () => {
 		expect(JSON.stringify(tp.transactions.find(t => t.id === newTransaction.id))).not.toEqual(oldTransaction);
 	});
 
-	
+	it('clears transactions', () => {
+
+		tp.clear();
+		expect(tp.transactions).toEqual([]);
+	});
+
+	describe('mixing valid and corrupt transactions', () => {
+
+		let validTransactions;
+
+		beforeEach(() => {
+
+			validTransactions = [...tp.transactions];
+
+			for(let i = 0; i<6;i++) {
+				wallet = new Wallet();
+				transaction = wallet.createTransaction('r4nd-4dr355', 30, bc, tp);
+				if (i%2==0){
+					transaction.input.amount = 99999;
+				} else {
+					validTransactions.push(transaction);
+				}
+			}
+		});
+
+		it('shows a difference between valid and corrupt transactions', () => {
+
+			expect(JSON.stringify(tp.transactions)).not.toEqual(JSON.stringify(validTransactions));
+		});
+
+		it('grabs valid transactions', () => {
+
+			expect(tp.validTransactions()).toEqual(validTransactions);
+		});
+	});	
 });
